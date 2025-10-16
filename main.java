bc1c2b10ae37334346e3b9fad3794b4c84f95aa9
tranbaonguyen-crypto <baonguyen27082004@gmail.com>
2025-10-16 23:00:48 +0700

    123123

--- main.java
+++ main.java
@@ -1,4 +1,6 @@
-public class main {
+import java.util.HashMap;
+
+public class Main {
     public static void main(String[] args) {
         //System.out.println(7/2);
         Node root = null;
@@ -9,9 +11,154 @@ public class main {
         root = addNode(root, 7);
         root = addNode(root, 8);
         root = addNode(root, 9);
-        deleteNode(root);
+
+        root = deleteNode(root);
 
         traverseNode(root);
+
+        // step 1
+        int numberOfNode = countNumberOfNode(root);
+        // step 2
+        int mid = findMidNode(numberOfNode);
+        // step 3
+        System.out.println("\nNode mid: " + getNodeMid(root, mid));
+
+//        int numberOfNode = countNumberOfNode(root);
+//        int halfOfNode = numberOfNode / 2;
+
+        String a = "ABCASDASDASD";
+        System.out.println("\nA: " + numberOfAppear(a, "A"));
+        System.out.println();
+
+        String b = "ABCDCBA";
+        String c = "ABCBA";
+        String d = "ABCDE";
+        checkSymmetry(b);
+        checkSymmetry(c);
+        checkSymmetry(d);
+
+    }
+    public void twoSum(List<Integer> numbers, int target) {
+        // a = [1,2,3,4,5,6,7,8,9];
+        // a = [1,1,2,3,4,4,5,6,7,8,9]
+        // tong no la 5 => (1,4) (1,4), (2,3)
+        // print 1 - 4 ; 4 - 1
+    }
+    // dem so luong phan tu trong chuoi
+    public int tinhGiaiThua(int n) {
+        // print n!
+        if (n<0)
+        if (n<0)
+
+        return 1;
+    }
+    public static int numberOfAppear(String string, String character) {
+        // ABCASDASDASD
+        // numberOfAppear(a, "A") =>4
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < string.length(); i++) {
+            if (map.get(string.charAt(i)) == null) {
+                map.put(string.charAt(i), 1);
+            } else {
+                map.put(string.charAt(i), map.get(string.charAt(i)) + 1);
+            }
+            // System.out.println("map = " + map);
+        }
+
+        // lay so luong ki tu
+        Integer value = map.get(character.charAt(0));
+
+        // tra ve -1 neu ko ton tai
+        if (value == null) {
+            return -1;
+        }
+        return value;
+    }
+
+    // kiem tra doi xưng
+    public static void checkSymmetry(String string){
+        // step 1: tao 2 con tro ( i ben trai, j ben phai) nguoc nhau
+        int i = 0;
+        int j = string.length() -1;
+        boolean result = true;
+        // step 2: so sanh 2 dau
+        // i<j vi i dau, j cuoi
+        while (i < j){
+            // neu ki tu vi tri i khac j thi tra ve sai
+            if (string.charAt(i) != string.charAt(j)){
+                result = false;
+                break;
+            }
+            // neu giong nhau thi tiep tuc i toi, j lui
+            i++;
+            j--;
+        }
+        if (result == true){
+            System.out.println( string + " doi xung");
+        } else {
+            System.out.println( string + " ko doi xung");
+        }
+    }
+
+    // tim node giua
+    // step 1: dem so node
+    // int numberOfNode = countNumberOfNode(root);
+    // tinh ham countNumberOfNode
+    // 6 node => count =6 ;7 node => count =7
+    // int count = 0;
+    // var tmp = root;
+    // while (tmp != null) {
+    //     count++;
+    //     tmp = tmp.nextNode;
+    // }
+    public static int countNumberOfNode(Node root) {
+        int count = 0;
+        var tmp = root;
+        while (tmp != null) {
+            count++;
+            tmp = tmp.nextNode;
+        }
+        return count;
+    }
+
+    // step 2: tim node giua tu numberOfNode
+    // int mid = findMidNode(numberOfNode);
+    // neu chan thi 6/2 = 3, le thi 7/2 + 1 = 4
+    // int mid = 0;
+    // if(count % 2 == 0){
+    //      mid = count / 2;
+    // } else {
+    //      mid = (count / 2) + 1;
+    // }
+    public static int findMidNode(int count) {
+        int mid;
+        if (count % 2 == 0) {
+            mid = count / 2;
+        } else {
+            mid = (count / 2) + 1;
+        }
+        return mid;
+    }
+
+    // step 3: duyet den vi tri giua
+    // getNodeMid(root, mid);
+    // var tmp = root;
+    // for (int i = 1; i < mid ; i++){
+    //      mid = 3 duyet den 2 xong nextnode = 3
+    //      tmp = tmp.nextNode;
+    // }
+    public static Node getNodeMid(Node root, int mid) {
+        if (root == null) {
+            return null;
+        }
+
+        var tmp = root;
+        for (int i = 1; i < mid; i++) {               // step 3
+            tmp = tmp.nextNode;
+        }
+
+        return tmp;
     }
 
     public static void traverseNode(Node root) {
@@ -37,28 +184,33 @@ public class main {
     }
 
     public static Node deleteNode(Node root) {
-        if (root == null) {
+        if (root == null) { // linkedlist empty
             return root;
         }
         // TODO : xoa thang cuoi cung trong linked list
         // 1 => 2 => 3
         // deleteNode() 1=>2
         // deleteNode() 1
-        var tmp = root;
-        while (tmp.nextNode.nextNode != null) {
-            tmp = tmp.nextNode;
-        }
-        tmp.nextNode = null;
         // deleteNode() null
         if (root.nextNode == null) {
             return null;
         }
+
+        Node head = null;
+        Node tmp = root;
+
+        while (tmp.nextNode != null) {
+            head = tmp;
+            tmp = tmp.nextNode;
+        }
+        head.nextNode = null;
+
         return root;
     }
 
-    public static int countNumberOfNode(Node root) {
-        return 0;
-    }
+//    public static int countNumberOfNode(Node root) {
+//        return 0;
+//    }
 
     static class Node {
         private int value;
