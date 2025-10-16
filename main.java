ebc04d01a99defac030367a5e2f9d02447139abd
tranbaonguyen-crypto <baonguyen27082004@gmail.com>
2025-10-16 23:03:27 +0700

    aaa

--- main.java
+++ main.java
@@ -40,7 +40,7 @@ public class Main {
     }
     public void twoSum(List<Integer> numbers, int target) {
         // a = [1,2,3,4,5,6,7,8,9];
-        // a = [1,1,2,3,4,4,5,6,7,8,9]
+        
         // tong no la 5 => (1,4) (1,4), (2,3)
         // print 1 - 4 ; 4 - 1
     }
